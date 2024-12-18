ca4e76acf5eb8de07c330a52aaae99171ffcc3e5
Mariam Azzouz <azzouzmariam3@gmail.com>
2024-12-18 20:16:21 +0100

    Add files via upload

---
+++ app/auth.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from app2.models.user import User
+from app2.config import db
+
+auth = Blueprint('main', __name__)
+
+@auth.route('/', methods=['GET', 'POST'])
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('avatar.index'))
+        flash('Email ou mot de passe incorrect')
+    return render_template('login.html')
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = generate_password_hash(request.form['password'], method='pbkdf2:sha256')
+        
+        if User.query.filter_by(email=email).first():
+            flash('Cet email est déjà enregistré')
+            return redirect(url_for('main.signup'))
+        
+        new_user = User(name=name, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        flash('Inscription réussie !')
+        return redirect(url_for('main.login'))
+    return render_template('signup.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.login')) 
