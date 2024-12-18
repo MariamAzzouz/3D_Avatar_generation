4e8f3f3a34792f268f85acc967a1a458ee998a2f
Mariam Azzouz <azzouzmariam3@gmail.com>
2024-12-18 20:14:48 +0100

    Add files via upload

---
+++ app/user.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+from app2.config import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    surname = db.Column(db.String(50))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200)) 
