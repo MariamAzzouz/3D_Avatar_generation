b5cac45d3f32c005461fbd9a1a1ed0304ba1b37d
Mariam Azzouz <azzouzmariam3@gmail.com>
2024-12-18 20:14:10 +0100

    Update and rename __init__.py to app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from app2.config import init_app, db, login_manager
-from app2.models.user import User
+from app.config import init_app, db, login_manager
+from app.models.user import User
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -10,9 +10,9 @@ def create_app():
     app = init_app()
 
     # Register blueprints
-    from app2.routes.auth import auth
-    from app2.routes.avatar import avatar
-    from app2.routes.clothing import clothing
+    from app.routes.auth import auth
+    from app.routes.avatar import avatar
+    from app.routes.clothing import clothing
     login_manager.login_view = 'main.login'
     #init_routes(app)
     app.register_blueprint(auth)
